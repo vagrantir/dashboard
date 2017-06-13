bfe5499c7aca33a7abf4bbc3a29007ba672dcc47
Daniil Petrov <vagrantir@gmail.com>
2017-06-13 13:31:33 +0300

    Service Worker alpha

--- aabsw.js
+++ aabsw.js
@@ -1,62 +1,108 @@
 /**
  * Anti AdBlock service
  */
+const magicWord = 'BWFNAWM'
 var rcCache = null
 const casheName = 'rc-cashe'
-
+const protectedUrls = [
+    '/api/cache/' + magicWord
+]
+function logs() {
+    [].forEach.call(arguments, function (v) {
+        try {
+            console.log(v)
+        } catch (e) {
+            console.log(e)
+        }
+    })
+}
 caches.open('rc-cashe')
     .then(function (cache) {
-    rcCache = cache;
-    console.log('SW1:SW1: Cache opened')
-    console.log(cache)
-})
+        rcCache = cache;
+        logs('SW1:SW1: Cache opened')
+        logs(cache)
+    })
 
 self.addEventListener('install', function (event) {
-    console.log('SW: Install event')
-    self.clients.claim() // get control on all already opened cliens
+    logs('SW: Install event')
+    // self.clients.claim() // get control on all already opened cliens
 });
 self.addEventListener('activate', function (event) {
-    console.log('SW: Activate event')
+    logs('SW: Activate event')
 })
 
 self.addEventListener('message', function (event) {
-    console.log('SW: get message', event.data)
+    logs('SW: get message', event.data)
 })
 
 self.addEventListener('fetch', function (event) {
-    console.log('SW1:fetch listener');
-    console.log(event.request.url);
-    console.log(event.request.headers);
+    logs('SW1:fetch listener');
+    logs(event.request.url);
     // var proxy = new Request('https://www.keycdn.com/blog/wp-content/uploads/2015/08/keycdn.png')
-    event.waitUntil(
-    fetch(new Request('adv.png'))
-        .catch(function(error){
-            console.log('SW1:fetch error', error)
-        })
-        .then(function(response){
-            console.log('SW1:fetch listener');
-            return response
-        })
+    event.respondWith(
+        getResource(event)
+            .then(function (r) {
+                return r
+            })
+            .catch(function (e) {
+                throw e
+            })
     )
 });
 
 self.addEventListener('sync', function (event) {
-    console.log('SW: Sync event')
+    logs('SW: Sync event')
 })
 
 self.addEventListener('push', function (event) {
-    console.log('SW: Push event')
+    logs('SW: Push event')
 })
 
-function loadScript(event) {
-    event.waitUntil(
-        answer = fetch(new Request(event.data.url))
-            .then(function (resp) {
-                return resp.text()
+function checkUrl(url) {
+    protectedUrls.forEach(function (v, i) {
+        if (url.match(RegExp(v, 'g'))) {
+            var key = (new URL(url)).searchParams.get('key')
+            if (key) {
+                url = decode(atob(key))
+            }
+            return url
+        }
+    })
+    return url
+}
+
+function decode(data) {
+    return data || '/adb/keycdn.png'
+}
+
+function getResource(event) {
+    var url = checkUrl(event.request.url)
+    return new Promise(function (resolve, reject) {
+        fetch(url)
+            .catch(function (error) {
+                logs(error, url)
+                fetch('/adb/keycdn.png')
+                    .catch(function (error) {
+                        logs(error, url)
+                        reject(error)
+                    })
+                    .then(function (response) {
+                        return resolve(response)
+                    })
             })
-            .then(function (text) {
-                event.ports[0].postMessage(text)
-                return text
+            .then(function (response) {
+                if (!response){
+                    fetch('/adb/smile.png')
+                        .catch(function (error) {
+                            logs(error, url)
+                            reject(error)
+                        })
+                        .then(function (response) {
+                            return resolve(response)
+                        })
+                } else {
+                    return resolve(response)
+                }
             })
-    )
+    })
 }
